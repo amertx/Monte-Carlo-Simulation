4cb2131cf7e18fb7c73b613d1ddc60801cc6043d
BryantH24 <gbhou5@gmail.com>
2020-06-08 17:26:20 -0400

    added comments for guidance, fixed form post request, fixed render on post

--- server.py
+++ server.py
@@ -2,7 +2,9 @@
 from flask import Flask, render_template, jsonify, url_for
 from flask import request
 
-import marketDataRetrieval as market
+# to have the webpage run the marketDataRetrieval needs to be correctly formatted
+# right now it gets messed up when its trying to
+# import marketDataRetrieval as market
 
 import requests
 
@@ -13,7 +15,9 @@ app = Flask(__name__, static_url_path='/static')
 @app.route('/', methods =['GET', 'POST'])
 def startPage():
     if request.method == 'POST':
-        return market.getData(tradingSymbol)
+        # inputData = market.getData(tradingSymbol)
+        # a flask route function returns html (or a html render_template)
+        return render_template('example.html', data = ["string", "variable", "anything else"])
     return render_template('index.html')
 
 
