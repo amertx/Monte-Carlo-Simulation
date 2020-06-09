b18d14df332ffcc56e44928e7837108c9000264b
amertx <amerdtx@gmail.com>
2020-06-08 23:18:22 -0500

    added two functions

--- marketDataRetrieval.py
+++ marketDataRetrieval.py
@@ -9,98 +9,69 @@ import matplotlib.pyplot as plt
 from scipy.stats import norm
 
 #ticker selection
-tradingSymbol = input("Enter a trading symbol: ")
-data = pd.DataFrame()
-data[tradingSymbol] = wb.DataReader(tradingSymbol, data_source='yahoo', start='2010-1-1')['Adj Close']
-#percent change of asset price
-log_returns = np.log(1+ data.pct_change())
-
-print("\nHead Data")
-print(log_returns.head())
-print("\nTail Data")
-print(log_returns.tail())
-
-#graph showing growth over time beginning from 2015
-data.plot(figsize = (10,6));
-plt.show()
-#graph of log returns of input ticker
-#returns are normally distributed and have a consistent mean
-log_returns.plot(figsize = (10,6))
-plt.show()
+def mainFunction(tradingSymbol):
+    data = pd.DataFrame()
+    data[tradingSymbol] = wb.DataReader(tradingSymbol, data_source='yahoo', start='2010-1-1')['Adj Close']
+    #percent change of asset price
+    log_returns = np.log(1+ data.pct_change())
 
 
-#calculations
-averageDailyReturn = log_returns.mean()
-variance = log_returns.var()
-drift = averageDailyReturn-(variance/2)
-standardDeviation = log_returns.std()
+    #graph showing growth over time beginning from 2015
+    data.plot(figsize = (10,6));
+    plt.show()
+    #graph of log returns of input ticker
+    #returns are normally distributed and have a consistent mean
+    log_returns.plot(figsize = (10,6))
+    plt.show()
 
 
-def retrievals():
-    #All retrievals are using Pandas Series Library
-    print("\nMean Retrieval")
-    print(averageDailyReturn)
+    #calculations
+    averageDailyReturn = log_returns.mean()
+    variance = log_returns.var()
+    drift = averageDailyReturn-(variance/2)
+    standardDeviation = log_returns.std()
 
-    #variance represents how spread out the data is from the average
-    #variance will represent the best way to represent the variability and volatility
-    print("\nVariance Retrieval")
-    print(variance)
-
-    #drift calculation
-    #Drift = averageDailyReturn-(variance/2)
-    print("\nDrift calculation")
-    print(drift)
-
-    #standard deviation calculation
-    print("\nStandard Deviation Retrieval")
-    print(standardDeviation)
-
-
-#calling of function
-retrievals()
 
 #Brownian Motion equation
 #r = drift + standardDeviation * (e^r)
 
 #prediction of future stock price based on simulation below using numpy for storing data into array
-np.array(drift)
-drift.values
-standardDeviation.values
 
-#Brownian motion variable correlating to the distance between the mean and the number of standard deviation
-norm.ppf(0.95)
+def prediction():
+    np.array(drift)
+    drift.values
+    standardDeviation.values
 
-#10 x 2 Matrix
-x = np.random.rand(10,2)
-norm.ppf(x)
+    #Brownian motion variable correlating to the distance between the mean and the number of standard deviation
+    norm.ppf(0.95)
 
-#stores distances from the mean value, 0, into the 10 x 2 matrix
-Z = norm.ppf(np.random.rand(10,2))
+    #10 x 2 Matrix
+    x = np.random.rand(10,2)
+    norm.ppf(x)
+
+    #stores distances from the mean value, 0, into the 10 x 2 matrix
+    Z = norm.ppf(np.random.rand(10,2))
 
 
 
-#time interval for the stock price forecast
-timeInterval = 30
-iterations = 5
+    #time interval for the stock price forecast
+    timeInterval = 365
+    iterations = 5
 
-#r = drift + standardDeviation * (e^r)
-#10 sets of 365 random future stock prices of the ticker symbol
-dailyReturns = np.exp(drift.values + standardDeviation.values * norm.ppf(np.random.rand(timeInterval,iterations)))
-print(dailyReturns)
+    #r = drift + standardDeviation * (e^r)
+    #10 sets of 365 random future stock prices of the ticker symbol
+    dailyReturns = np.exp(drift.values + standardDeviation.values * norm.ppf(np.random.rand(timeInterval,iterations)))
 
-#returns into price points
-presentPrice = data.iloc[-1]
-print(presentPrice)
-priceList = np.zeros_like(dailyReturns)
-priceList[0] = presentPrice
-#iteration for the time interavl of 365
-for t in range(1, timeInterval):
-    priceList[t] = priceList[t-1] * dailyReturns[t]
 
-print(priceList[-1])
-#showcases 10 paths of the future stock price
-plt.figure(figsize =(10,6))
-plt.plot(priceList)
-plt.show()
+    #returns into price points
+    presentPrice = data.iloc[-1]
+    priceList = np.zeros_like(dailyReturns)
+    priceList[0] = presentPrice
+    #iteration for the time interavl of 365
+    for t in range(1, timeInterval):
+        priceList[t] = priceList[t-1] * dailyReturns[t]
 
-exit()
+    #showcases 10 paths of the future stock price
+    plt.figure(figsize =(10,6))
+    plt.plot(priceList)
+    plt.show()
